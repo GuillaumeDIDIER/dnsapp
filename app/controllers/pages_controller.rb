46d7210e41f8ca23885643737fffc9a6780bda17
Johann-Michael THIEBAUT <johann.thiebaut@gmail.com>
2012-01-28 11:21:53 +0100

    Added an 'about ip' page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,19 @@ class PagesController < ApplicationController
     @title = "Aide"
   end
 
+  def about_ip
+    if params[:ip].nil?
+      path = url_for( :controller => 'pages',
+                    :action => 'about_ip',
+                    :ip => current_ip,
+                    :only_path => true )
+      redirect_to path and return
+    end
+
+    @ip = params[:ip]
+    @zone = find_zone @ip
+    @name = get_domain_name_from_ip @ip
+    @title = "À propos de #{params[:ip]}"
+  end
+
 end
