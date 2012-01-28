d8c4ed4046fccce85a558f1f52a3da8b122b5257
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2012-01-28 22:20:38 +0100

    Translation changed

--- app/controllers/reverse_dns_records_controller.rb
+++ app/controllers/reverse_dns_records_controller.rb
@@ -44,7 +44,7 @@ class ReverseDnsRecordsController < ApplicationController
     end
 
     def record_field_names
-      ["nom", "zone", "type", "données"]
+      ["nom", "zone", "type", "cible"]
     end
 
     def search_types
