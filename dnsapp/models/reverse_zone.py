1cf74d65886bab6a0d89c93d2ef692c26e1a6dba
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2012-12-26 22:29:30 +0100

    Add useful functions to manipulate records of reverse zones
    Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

--- dnsapp/models/reverse_zone.py
+++ dnsapp/models/reverse_zone.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from django.db import models
-from zone import Zone
+from dnsapp.models.zone import Zone
+from dnsapp.models import ip_address
+
+
+class ReverseZoneManager(models.Manager):
+    """Provide some functions to use IP addresses"""
+
+    def get_by_ip(self, ip):
+        """Get the best reverse zone which matches this IP address
+
+        Return ReverseZone instance
+        """
+        nums = ip.split('.')
+        if len(nums) != 4:
+            raise self.model.DoesNotExist("Invalid IP address")
+        ip_prefixes = (
+            '%d.' % int(nums[0]),
+            '%d.%d.' % (int(nums[0]), int(nums[1])),
+            '%d.%d.%d.' % (int(nums[0]), int(nums[1]), int(nums[2])),
+            )
+        return self.filter(ip_prefix__in=ip_prefixes).get()
 
 
 class ReverseZone(models.Model):
@@ -16,8 +36,28 @@ class ReverseZone(models.Model):
     ip_prefix = models.CharField(max_length=16, primary_key=True)
     ip_prefix.help_text = "Prefix of IP addresses in this zone"
 
+    objects = ReverseZoneManager()
+
     def __unicode__(self):
         return self.ip_prefix
+
+    def host2ip(self, host):
+        """Get IP address for the given reversed host name
+
+        Return None if the result is not a valid IP address
+        """
+        nums = host.split('.')
+        nums.reverse()
+        ip = self.ip_prefix + '.'.join(nums)
+        return ip if ip_address.is_ipv4(ip) else None
+
+    def ip2host(self, ip):
+        """Get reverse host name from an IP address, or None"""
+        if not ip.startswith(self.ip_prefix) or not ip_address.is_ipv4(ip):
+            return None
+        nums = ip[len(self.ip_prefix):].split('.')
+        nums.reverse()
+        return '.'.join(nums)
 
 
 class ReverseZoneAdmin(admin.ModelAdmin):
--- dnsapp/tests/models.py
+++ dnsapp/tests/models.py
@@ -1,8 +1,85 @@
 from django.test import TestCase
-from dnsapp.models import zone
+from dnsapp.models import ReverseZone, ip_address, zone
 
 
-class SimpleTest(TestCase):
+class IPAddressTest(TestCase):
+
+    def test_is_ipv4(self):
+        # Valid IPv4
+        self.assertTrue(ip_address.is_ipv4('127.0.0.1'))
+        # Nothing or bad type
+        self.assertFalse(ip_address.is_ipv4(None))
+        self.assertFalse(ip_address.is_ipv4(1))
+        self.assertFalse(ip_address.is_ipv4(''))
+        # Too short
+        self.assertFalse(ip_address.is_ipv4('42'))
+        self.assertFalse(ip_address.is_ipv4('13.37'))
+        # Too many characters
+        self.assertFalse(ip_address.is_ipv4('127.0.0.1.42'))
+        # Invalid characters
+        self.assertFalse(ip_address.is_ipv4('127.0.0.1a'))
+        # Invalid number
+        self.assertFalse(ip_address.is_ipv4('127.0.0.256'))
+
+    def test_ptr2ip(self):
+        # Valid calls
+        self.assertEquals(ip_address.ptr2ip('4.3.2.1.in-addr.arpa.'),
+                          '1.2.3.4')
+        self.assertEquals(ip_address.ptr2ip('42.in-addr.arpa.'), '42.')
+        self.assertEquals(ip_address.ptr2ip('42.in-addr.arpa'), '42.')
+        self.assertEquals(ip_address.ptr2ip('1.42.in-addr.arpa'), '42.1.')
+        # Invalid calls
+        self.assertIsNone(ip_address.ptr2ip('.1.in-addr.arpa.'))
+        self.assertIsNone(ip_address.ptr2ip('2..1.in-addr.arpa.'))
+        self.assertIsNone(ip_address.ptr2ip('5.4.3.2.1.in-addr.arpa.'))
+        self.assertIsNone(ip_address.ptr2ip('4.3.2.1a.in-addr.arpa.'))
+
+    def test_ip2ptr(self):
+        # Valid calls
+        self.assertEquals(ip_address.ip2ptr('1.2.3.4'),
+                          '4.3.2.1.in-addr.arpa.')
+        self.assertEquals(ip_address.ip2ptr('42.'), '42.in-addr.arpa.')
+        self.assertEquals(ip_address.ip2ptr('42.1.'), '1.42.in-addr.arpa.')
+        # Invalid calls
+        self.assertIsNone(ip_address.ip2ptr('.1.'))
+        self.assertIsNone(ip_address.ip2ptr('1..2'))
+        self.assertIsNone(ip_address.ip2ptr('1.2.3.4.5'))
+        self.assertIsNone(ip_address.ip2ptr('1.2.3.4a'))
+
+
+class ReverseZoneTest(TestCase):
+    fixtures = ['tests.json']
+    multi_db = True
+
+    def setUp(self):
+        self.revzone127 = ReverseZone.objects.get(ip_prefix='127.')
+
+    def test_get_by_ip(self):
+        # Test normal call
+        self.assertEquals(
+            ReverseZone.objects.get_by_ip('127.0.0.1'),
+            self.revzone127)
+        # Test erroneous call
+        self.assertRaises(
+            ReverseZone.DoesNotExist,
+            ReverseZone.objects.get_by_ip, '127.0.0')
+
+    def test_host2ip(self):
+        # Test normal call
+        self.assertEquals(self.revzone127.host2ip('42.0.0'), '127.0.0.42')
+        # Test invalid resulting IP
+        self.assertIsNone(self.revzone127.host2ip('42.0'))
+        self.assertIsNone(self.revzone127.host2ip('42.0a'))
+
+    def test_ip2host(self):
+        # Test normal call
+        self.assertEquals(self.revzone127.ip2host('127.0.0.42'), '42.0.0')
+        # Test invalid IP
+        self.assertIsNone(self.revzone127.ip2host('127.0.42'))
+        self.assertIsNone(self.revzone127.ip2host('127.0.0.42a'))
+
+
+class ZoneTest(TestCase):
 
     def test_zone_serial2tuple(self):
         """Test serial2tuple function"""
